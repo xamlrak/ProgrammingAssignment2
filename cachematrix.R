3f808e2cd35d625f5234567cef0b3964f68d3435
xamlrak <kgcorporan@gmail.com>
2015-03-16 22:31:42 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## With these functions we create a special matrix then either bring its previously calculated inverse stored in the Cache 
+## or calculates it if it has not been done before
 
-## Write a short comment describing this function
+## Creates the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+	r <- NULL
+	set <- function(y) {
+		x <<- y
+		r <<- NULL
+	}
+	get <- function() x
+	setsolve <- function(solve) r <<- solve
+	getsolve <- function() r
+	list(set = set, get = get,
+	     setsolve = setsolve,
+	     getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Looks up the matrixs inverse or calculates it if not found
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function (x, ...){
+	r <- x$getsolve()
+	if(!is.null(r)) {
+		message("getting cached data")
+		return(r)
+	}
+	data <- x$get()
+	r <- solve(data, ...)
+	x$setsolve(r)
+	r
 }
